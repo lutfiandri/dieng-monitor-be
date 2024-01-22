401a5817d53ef6aae757261e6d6586096c5477e6
lutfiandri <lutfiandri232@gmail.com>
2024-01-22 23:35:19 +0700

    feat: export to excel

--- controller/device-controller.js
+++ controller/device-controller.js
@@ -1,5 +1,7 @@
 const { FieldValue } = require('firebase-admin/firestore');
 const { db } = require('../config/firebase');
+const path = require('path');
+const ExcelJS = require('exceljs');
 
 const upsertData = async (req, res) => {
   try {
@@ -24,6 +26,59 @@ const upsertData = async (req, res) => {
   }
 };
 
+const exportExcel = async (req, res) => {
+  try {
+    const deviceId = req.params.id;
+
+    const doc = await db.collection('devices').doc(deviceId).get();
+    if (!doc.exists) {
+      return res.status(404).send('device not found');
+    }
+
+    const data = doc.data();
+    const deviceData = data.data;
+
+    const formattedData = deviceData.map((d, i) => ({
+      no: i + 1,
+      timestamp: d.timestamp.toDate().toISOString(),
+      co2: d.co2,
+      temperature: d.temperature,
+    }));
+
+    const workbook = new ExcelJS.Workbook();
+    const worksheet = workbook.addWorksheet('Sheet 1');
+
+    worksheet.columns = [
+      { header: 'No.', key: 'no', width: 5 },
+      { header: 'Timestamp', key: 'timestamp', width: 30 },
+      { header: 'CO2', key: 'co2', width: 15 },
+      { header: 'Suhu (Celsius)', key: 'temperature', width: 15 },
+    ];
+
+    worksheet.addRows(formattedData);
+
+    const directory = 'files';
+    const filename = `Data Dieng Monitor - ${new Date().toISOString()}.xlsx`;
+
+    const filepath = path.join(__dirname, '..', directory, filename);
+    console.log(filepath);
+    // const filepath = `${directory}/Data Dieng Monitor - ${new Date().toISOString()}.xlsx`;
+
+    await workbook.xlsx.writeFile(filepath).then(() => {
+      res.setHeader(
+        'Content-Type',
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+      );
+      res.setHeader('Content-Disposition', `attachment; filename=${filename}`);
+
+      return res.sendFile(filepath);
+    });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
 module.exports = {
   upsertData,
+  exportExcel,
 };
--- index.js
+++ index.js
@@ -9,10 +9,12 @@ const deviceController = require('./controller/device-controller');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('files'));
 
 app.get('/', (req, res) => res.send('welcome to dieng-monitor'));
 
 app.post('/devices/:id/data', deviceController.upsertData);
+app.get('/devices/:id/excel', deviceController.exportExcel);
 
 app.listen(env.AppPort, () =>
   console.log('server started on port', env.AppPort)
